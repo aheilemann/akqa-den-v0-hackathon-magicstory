737f79279d9134a1a4b599a01bc1e4a9fd33c64e
danielrhjensen <daniel.jensen@akqa.com>
2025-02-25 12:20:11 +0100

    Added `/create` page
    Added it to the "protected" folder (for now) to ensure user is logged-in
    before using the create functionality/page

---
+++ app/protected/create/page.tsx
@@ -0,0 +1,11 @@
+import { StoryBuilder } from "@/components/organisms/story-builder";
+
+const CreatePage = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <StoryBuilder />
+    </div>
+  );
+};
+
+export default CreatePage;
