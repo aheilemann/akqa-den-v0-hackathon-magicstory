25999cccbaa0cbfd1f08755dcc0fd19d6b75c095
Nikolaj Pejstrup <nikolajpaystrup@gmail.com>
2025-03-04 09:09:25 +0100

    Use remotePatterns in next cfg

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "eujhdaaodhbravffsyrx.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "eujhdaaodhbravffsyrx.supabase.co",
+        pathname: "**",
+      },
+    ],
   },
 };
 
